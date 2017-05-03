847c855a93f31e68baabbe2d85f941a51aaec257
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-05-03 20:23:34 +0100

    Updates

--- Source/CodeSmithyUMLWebWidget.js
+++ Source/CodeSmithyUMLWebWidget.js
@@ -81,3 +81,30 @@ function visibilityStringToSymbol(visibility) {
   }
   return symbol
 }
+
+function drawInheritanceRelationship(svg, classboxes, baseclass, derivedclass) {
+  var g = svg.group().addClass("UMLInheritanceRelationship")
+  var bbox1 = classboxes[baseclass].bbox();
+  var bbox2 = classboxes[derivedclass].bbox();
+
+  var t1 = "" + bbox1.cx + "," + (bbox1.y + bbox1.height) + " " +
+    (bbox1.cx - 10) + "," + (bbox1.y + bbox1.height + 12) + " " +
+    (bbox1.cx + 10) + "," + (bbox1.y + bbox1.height + 12)                
+  g.polygon(t1)
+
+  g.line(bbox1.cx, bbox1.y + bbox1.height + 12, bbox2.cx, bbox2.y)
+}
+
+function drawCompositionRelationship(svg, classboxes, containingclass, containedclass) {
+  var g = svg.group().addClass("UMLCompositionRelationship")
+  var bbox1 = classboxes[item.relationship.containingclass].bbox();
+  var bbox2 = classboxes[item.relationship.containedclass].bbox();
+
+  var t1 = "" + (bbox1.x + bbox1.width) + "," + bbox1.cy + " " +
+    (bbox1.x + bbox1.width + 10) + "," + (bbox1.cy - 8) + " " +
+    (bbox1.x + bbox1.width + 20) + "," + bbox1.cy + " " +
+    (bbox1.x + bbox1.width + 10) + "," + (bbox1.cy + 8)
+  g.polygon(t1)
+              
+  g.line(bbox1.x + bbox1.width + 20, bbox1.cy, bbox2.x, bbox2.cy)
+}
