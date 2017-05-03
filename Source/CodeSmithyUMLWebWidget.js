b44693c631737e5ed4ed8ce1752bcf965f82d21d
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-05-03 23:13:21 +0100

    Updates

--- Source/CodeSmithyUMLWebWidget.js
+++ Source/CodeSmithyUMLWebWidget.js
@@ -89,86 +89,87 @@ function drawCompositionRelationship(svg, classboxes, containingclass, contained
 }
 
 function ClassBox(svg, classDescription, interactive, classboxStyle, layout) {
-  this.def = addClassDef(svg.defs(), classDescription, interactive, classboxStyle)
+
+  this.createDef = function(defs, classInfo, interactive, style) {
+    var classGroup = defs.group().addClass("UMLClass")
+  
+    if (interactive) {
+      classGroup.click(function() {
+        this.text("hhe")
+      })
+    }
+
+    var classBoxWidth = 0
+    var classBoxHeight = style["margin-top"]
+
+    var classNameDef = defs.text(classInfo.name).move(style["margin-left"], classBoxHeight)
+    classBoxWidth = Math.max(classBoxWidth, classNameDef.bbox().width)
+    classBoxHeight += (classNameDef.bbox().height + style["margin-bottom"])
+
+    var line1YPos = classBoxHeight;
+ 
+    classBoxHeight += style["margin-top"]
+
+    var attributeDefs = []
+    for (var i = 0; i < classInfo.attributes.length; i++) {
+      var attrItem = classInfo.attributes[i]
+      var attrText = visibilityStringToSymbol(attrItem.visibility) + attrItem.name
+      var attributeDef = defs.text(attrText).move(style["margin-left"], classBoxHeight)
+      attributeDefs.push(attributeDef)
+      classBoxWidth = Math.max(classBoxWidth, attributeDef.bbox().width)
+      classBoxHeight += attributeDef.bbox().height;
+    }
+
+    classBoxHeight += style["margin-bottom"]
+
+    var line2YPos = classBoxHeight;
+
+    classBoxHeight += style["margin-top"]
+
+    var operationDefs = []
+    for (var i = 0; i < classInfo.operations.length; i++) {
+      var opItem = classInfo.operations[i]
+      var opText = visibilityStringToSymbol(opItem.visibility) + opItem.name
+      var operationDef = defs.text(opText).move(style["margin-left"], classBoxHeight)
+      operationDefs.push(operationDef)
+      classBoxWidth = Math.max(classBoxWidth, operationDef.bbox().width)
+      classBoxHeight += operationDef.bbox().height;
+    }
+
+    // According to the UML standard the class name must be
+    // centered so center it
+    if (classBoxWidth > classNameDef.bbox().width) {
+      classNameDef.dx((classBoxWidth - classNameDef.bbox().width)/2)
+    }
+
+    classBoxWidth += (style["margin-left"] + style["margin-right"])
+    classBoxHeight += style["margin-bottom"]
+
+    var classBoxRect = classGroup.rect(classBoxWidth, classBoxHeight).move(0,0)
+
+    classGroup.use(classNameDef)
+    classGroup.move(1,1)
+
+    classGroup.line(0, line1YPos, classBoxWidth, line1YPos)
+
+    for (var i = 0; i < attributeDefs.length; i++) {
+      classGroup.use(attributeDefs[i])
+    }
+
+    classGroup.line(0, line2YPos, classBoxWidth, line2YPos)
+
+    for (var i = 0; i < operationDefs.length; i++) {
+      classGroup.use(operationDefs[i])
+    }
+
+    return classGroup
+  }
+
+  this.def = this.createDef(svg.defs(), classDescription, interactive, classboxStyle)
   if (layout.positions[classDescription.name]) {
     this.def.move(layout.positions[classDescription.name].x, layout.positions[classDescription.name].y)
   }
   this.svg = svg.use(this.def)
-}
-
-function addClassDef(defs, classInfo, interactive, style) {
-  var classGroup = defs.group().addClass("UMLClass")
-  
-  if (interactive) {
-    classGroup.click(function() {
-      this.text("hhe")
-    })
-  }
-
-  var classBoxWidth = 0
-  var classBoxHeight = style["margin-top"]
-
-  var classNameDef = defs.text(classInfo.name).move(style["margin-left"], classBoxHeight)
-  classBoxWidth = Math.max(classBoxWidth, classNameDef.bbox().width)
-  classBoxHeight += (classNameDef.bbox().height + style["margin-bottom"])
-
-  var line1YPos = classBoxHeight;
- 
-  classBoxHeight += style["margin-top"]
-
-  var attributeDefs = []
-  for (var i = 0; i < classInfo.attributes.length; i++) {
-    var attrItem = classInfo.attributes[i]
-    var attrText = visibilityStringToSymbol(attrItem.visibility) + attrItem.name
-    var attributeDef = defs.text(attrText).move(style["margin-left"], classBoxHeight)
-    attributeDefs.push(attributeDef)
-    classBoxWidth = Math.max(classBoxWidth, attributeDef.bbox().width)
-    classBoxHeight += attributeDef.bbox().height;
-  }
-
-  classBoxHeight += style["margin-bottom"]
-
-  var line2YPos = classBoxHeight;
-
-  classBoxHeight += style["margin-top"]
-
-  var operationDefs = []
-  for (var i = 0; i < classInfo.operations.length; i++) {
-    var opItem = classInfo.operations[i]
-    var opText = visibilityStringToSymbol(opItem.visibility) + opItem.name
-    var operationDef = defs.text(opText).move(style["margin-left"], classBoxHeight)
-    operationDefs.push(operationDef)
-    classBoxWidth = Math.max(classBoxWidth, operationDef.bbox().width)
-    classBoxHeight += operationDef.bbox().height;
-  }
-
-  // According to the UML standard the class name must be
-  // centered so center it
-  if (classBoxWidth > classNameDef.bbox().width) {
-    classNameDef.dx((classBoxWidth - classNameDef.bbox().width)/2)
-  }
-
-  classBoxWidth += (style["margin-left"] + style["margin-right"])
-  classBoxHeight += style["margin-bottom"]
-
-  var classBoxRect = classGroup.rect(classBoxWidth, classBoxHeight).move(0,0)
-
-  classGroup.use(classNameDef)
-  classGroup.move(1,1)
-
-  classGroup.line(0, line1YPos, classBoxWidth, line1YPos)
-
-  for (var i = 0; i < attributeDefs.length; i++) {
-    classGroup.use(attributeDefs[i])
-  }
-
-  classGroup.line(0, line2YPos, classBoxWidth, line2YPos)
-
-  for (var i = 0; i < operationDefs.length; i++) {
-    classGroup.use(operationDefs[i])
-  }
-
-  return classGroup
 }
 
 // Converts the visibility from the user string provided
