e3eb02f367ca0a1fcaa25fb26e48ea5cd3541e80
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-07-30 11:52:58 +0100

    Updates

--- test/index.js
+++ test/index.js
@@ -1,4 +1,10 @@
+'use strict'
+
+var AddClassDiagramTests = require("./ClassDiagramTests.js")
 var tf = require("ishiko-test-framework")
 
 let theTestHarness = new tf.TestHarness("codesmithy-umlwebwidget")
+
+AddClassDiagramTests(theTestHarness)
+
 theTestHarness.run()
