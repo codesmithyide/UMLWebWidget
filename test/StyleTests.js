d2d5d68499539c109e4be47250b2bcf536dcfe55
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-08-16 10:31:52 +0100

    Updates

---
+++ test/StyleTests.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var UMLWebWidget = require("../dist/codesmithy-umlwebwidget.js")
+var tf = require("ishiko-test-framework")
+
+module.exports = function(theTestHarness) {
+    let styleSequence = theTestHarness.appendTestSequence("Style tests")
+
+    new tf.FunctionBasedTest("Creation test 1", StyleCreationTest1, styleSequence)
+}
+
+function StyleCreationTest1(resolve) {
+    let style = new UMLWebWidget.Style()
+    resolve(tf.TestResultOutcome.ePassed)
+}
