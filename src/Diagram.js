cc135196932969416b5f5af407604dcde1718749
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-08-18 00:16:35 +0100

    Updates

--- src/Diagram.js
+++ src/Diagram.js
@@ -107,8 +107,8 @@ export class Diagram {
                 if (layout.classboxpositions[className]) {
                     newClassBox.move(layout.classboxpositions[className].x, layout.classboxpositions[className].y)
                 }
-                newClassBox.layers.getLayer("shape").write()
-                newClassBox.layers.getLayer("text").write()
+                newClassBox.getLayers().getLayer("shape").write()
+                newClassBox.getLayers().getLayer("text").write()
             } else if (item.relationship) {
                 let classbox1
                 let classbox2
