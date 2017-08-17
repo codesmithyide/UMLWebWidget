28b8138382e60177540f900319de824b40db424c
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-08-17 20:32:38 +0100

    Test updates

--- test/ClassBoxTests.js
+++ test/ClassBoxTests.js
@@ -4,11 +4,15 @@ var window = require("svgdom")
 var SVG = require("svg.js")(window)
 var UMLWebWidget = require("../dist/codesmithy-umlwebwidget.js")
 var tf = require("ishiko-test-framework")
+var TestUtils = require("./TestUtils.js")
 
 module.exports = function(theTestHarness) {
     let classboxSequence = theTestHarness.appendTestSequence("ClassBox tests")
 
     new tf.FunctionBasedTest("Creation test 1", ClassBoxCreationTest1, classboxSequence)
+
+    new tf.FileComparisonTest("getLayers test 1", ClassBoxGetLayersTest1, classboxSequence)
+    new tf.FileComparisonTest("getLayers test 2", ClassBoxGetLayersTest2, classboxSequence)
 }
 
 function ClassBoxCreationTest1(resolve) {
@@ -24,5 +28,43 @@ function ClassBoxCreationTest1(resolve) {
     }
     let style = new UMLWebWidget.Style()
     let classbox = new UMLWebWidget.ClassBox(svg, classDescription, false, style)
+    resolve(tf.TestResultOutcome.ePassed)
+}
+
+function ClassBoxGetLayersTest1(resolve, reject, test) {
+    let svg = SVG(window.document.createElement("div"))
+
+    let classDescription = {
+        "name": "MyClass",
+        "attributes":
+            [
+            ],
+        "operations":
+            [
+            ]
+    }
+    let style = new UMLWebWidget.Style()
+    let classbox = new UMLWebWidget.ClassBox(svg, classDescription, false, style)
+
+    let layers = classbox.getLayers()
+    layers.layers["shape"].write()
+    layers.layers["text"].write()
+
+    TestUtils.exportSVGToHTML(svg, __dirname + "/output/ClassBoxGetLayersTest1.html", true)
+
+    test.setOutputFilePath(__dirname + "/output/ClassBoxGetLayersTest1.html")
+    test.setReferenceFilePath(__dirname + "/reference/ClassBoxGetLayersTest1.html")
+
+    resolve(tf.TestResultOutcome.ePassed)
+}
+
+function ClassBoxGetLayersTest2(resolve, reject, test) {
+    let svg = SVG(window.document.createElement("div"))
+
+    TestUtils.exportSVGToHTML(svg, __dirname + "/output/ClassBoxGetLayersTest2.html", true)
+
+    test.setOutputFilePath(__dirname + "/output/ClassBoxGetLayersTest2.html")
+    test.setReferenceFilePath(__dirname + "/reference/ClassBoxGetLayersTest2.html")
+
     resolve(tf.TestResultOutcome.ePassed)
 }
