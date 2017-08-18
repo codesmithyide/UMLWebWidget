fb0b329a7677a1e1cd700c15b2fa7378cbefd5e8
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-08-18 22:39:25 +0100

    Add ConnectionPoint class

---
+++ src/ConnectionPoint.js
@@ -0,0 +1,29 @@
+'use strict'
+
+import { DiagramElement } from "./DiagramElement.js"
+
+/**
+  <p>
+    The point where an element and a connector meet.
+  </p>
+
+  <p>
+    Although it derives from {@link DiagramElement} this
+    element will probably be invisible to the user. 
+    However it may be useful to make the connection points
+    visible under some circumstances like for instance when
+    the diagram is being edited.
+  </p>
+
+  @extends DiagramElement
+  @property {DiagramElement} this.element - The element.
+*/
+class ConnectionPoint extends DiagramElement {
+
+    constructor(svg, element) {
+        super(svg)
+        this.element = element
+    }
+}
+
+export { ConnectionPoint }
