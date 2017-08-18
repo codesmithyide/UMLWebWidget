567635bfe8d120966366836a2f6dd0d58e5c3c44
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-08-18 23:04:13 +0100

    Added ConnectionPoint tests

---
+++ test/ConnectionPointTests.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var UMLWebWidget = require("../dist/codesmithy-umlwebwidget.js")
+var tf = require("ishiko-test-framework")
+
+module.exports = function(theTestHarness) {
+    let connectionPointSequence = theTestHarness.appendTestSequence("ConnectionPoint tests")
+
+    new tf.FunctionBasedTest("Creation test 1", ConnectionPointCreationTest1, connectionPointSequence)
+}
+
+function ConnectionPointCreationTest1(resolve) {
+    let point = new UMLWebWidget.ConnectionPoint()
+    resolve(tf.TestResultOutcome.ePassed)
+}
