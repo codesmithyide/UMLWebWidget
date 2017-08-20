1992ac46ab7913152fb49c22172e230be5dc8a37
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-08-20 19:36:46 +0100

    Test updates

--- test/TestUtils.js
+++ test/TestUtils.js
@@ -21,9 +21,9 @@ function createDirectory(path) {
 function exportSVGToHTML(svg, path, stylesheet = false) {
     let data = ""
     if (stylesheet) {
-        data = "<html><body><head><link rel=\"stylesheet\" href=\"../../StyleSheets/CodeSmithyUMLWebWidget.css\"></link></head><div class=\"CodeSmithyUMLWebWidget\">" + svg.svg() + "</div></body></html>"
+        data = "<!DOCTYPE html><html><head><link rel=\"stylesheet\" href=\"../../StyleSheets/CodeSmithyUMLWebWidget.css\"></link></head><body><div class=\"CodeSmithyUMLWebWidget\">" + svg.svg() + "</div></body></html>"
     } else {
-        data = "<html><body><div>" + svg.svg() + "</div></body></html>"
+        data = "<!DOCTYPE html><html><body><div>" + svg.svg() + "</div></body></html>"
     }
     fs.writeFileSync(path, prettyprint.xml(data))
 }
