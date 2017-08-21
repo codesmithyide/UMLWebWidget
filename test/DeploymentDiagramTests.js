432367b95cf5a98807ea647380334ff54fdc5806
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-08-21 23:30:38 +0100

    Test updates

---
+++ test/DeploymentDiagramTests.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var UMLWebWidget = require("../dist/codesmithy-umlwebwidget.js")
+var tf = require("ishiko-test-framework")
+
+module.exports = function(theTestHarness) {
+    let deploymentDiagramSequence = theTestHarness.appendTestSequence("Deployment diagram tests")
+}
---
+++ test/SequenceDiagramTests.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var UMLWebWidget = require("../dist/codesmithy-umlwebwidget.js")
+var tf = require("ishiko-test-framework")
+
+module.exports = function(theTestHarness) {
+    let sequenceDiagramSequence = theTestHarness.appendTestSequence("Sequence diagram tests")
+}
