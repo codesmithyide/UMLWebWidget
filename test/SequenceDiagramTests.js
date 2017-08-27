8a03a38af74bd64834e5cd2c95bfa31a9f225898
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-08-27 11:04:58 +0100

    Test updates

--- test/SequenceDiagramTests.js
+++ test/SequenceDiagramTests.js
@@ -1,8 +1,43 @@
 'use strict'
 
+var window = require("svgdom")
+var SVG = require("svg.js")(window)
 var UMLWebWidget = require("../dist/codesmithy-umlwebwidget.js")
 var tf = require("ishiko-test-framework")
+var TestUtils = require("./TestUtils.js")
 
 module.exports = function(theTestHarness) {
     let sequenceDiagramSequence = theTestHarness.appendTestSequence("Sequence diagram tests")
+
+    TestUtils.createDirectory(__dirname + "/output/sequencediagramtests")
+
+    new tf.FileComparisonTest("createFromJSON test 1", SequenceDiagramCreateFromJSONTest1, sequenceDiagramSequence)
+}
+
+function SequenceDiagramCreateFromJSONTest1(resolve, reject, test) {
+    let svg = SVG(window.document.createElement("div"))
+    let sequenceDiagram = new UMLWebWidget.Diagram()
+    sequenceDiagram.createFromJSON(svg, {
+        "sequencediagram":
+          [
+              { 
+                  "lifeline":
+                      {
+                          "name": "Customer"
+                      }
+              }
+          ]
+    })
+    let elementKeys = Object.keys(sequenceDiagram.diagramDescription)
+    let lifelinesKeys = Object.keys(sequenceDiagram.lifelines)
+    if ((elementKeys.length == 1) && (lifelinesKeys.length == 1)) {
+        TestUtils.exportSVGToHTML(svg, __dirname + "/output/sequencediagramtests/SequenceDiagramCreateFromJSONTest1.html", true)
+
+        test.setOutputFilePath(__dirname + "/output/sequencediagramtests/SequenceDiagramCreateFromJSONTest1.html")
+        test.setReferenceFilePath(__dirname + "/reference/sequencediagramtests/SequenceDiagramCreateFromJSONTest1.html")
+
+        resolve(tf.TestResultOutcome.ePassed)
+    } else {
+        resolve(tf.TestResultOutcome.eFailed)
+    }
 }
