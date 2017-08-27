125fe054bbaacbc8a4798c921a6ed08123f39ad8
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-08-27 11:38:03 +0100

    Test updates

--- test/SequenceDiagramTests.js
+++ test/SequenceDiagramTests.js
@@ -13,6 +13,7 @@ module.exports = function(theTestHarness) {
 
     new tf.FileComparisonTest("createFromJSON test 1", SequenceDiagramCreateFromJSONTest1, sequenceDiagramSequence)
     new tf.FileComparisonTest("createFromJSON test 2", SequenceDiagramCreateFromJSONTest2, sequenceDiagramSequence)
+    new tf.FileComparisonTest("createFromJSON test 3", SequenceDiagramCreateFromJSONTest3, sequenceDiagramSequence)
 }
 
 function SequenceDiagramCreateFromJSONTest1(resolve, reject, test) {
@@ -80,6 +81,63 @@ function SequenceDiagramCreateFromJSONTest2(resolve, reject, test) {
 
         test.setOutputFilePath(__dirname + "/output/sequencediagramtests/SequenceDiagramCreateFromJSONTest2.html")
         test.setReferenceFilePath(__dirname + "/reference/sequencediagramtests/SequenceDiagramCreateFromJSONTest2.html")
+
+        resolve(tf.TestResultOutcome.ePassed)
+    } else {
+        resolve(tf.TestResultOutcome.eFailed)
+    }
+}
+
+function SequenceDiagramCreateFromJSONTest3(resolve, reject, test) {
+    let svg = SVG(window.document.createElement("div"))
+
+    let layout = {
+        "lifelinepositions": {
+            "Customer": { "x": 1, "y": 1 },
+            "Shopkeeper": { "x": 150, "y": 1 }
+        }
+    }
+
+    let sequenceDiagram = new UMLWebWidget.Diagram()
+    sequenceDiagram.createFromJSON(svg, {
+        "sequencediagram":
+            [
+                { 
+                    "lifeline":
+                        {
+                            "name": "Customer"
+                        }
+                },
+                {
+                    "lifeline":
+                        {
+                            "name": "Shopkeeper"
+                        }
+                },
+                {
+                    "messages":
+                        [
+                            {
+                                "synchronousmessage":
+                                    {
+                                        "name": "pay",
+                                        "caller": "Customer",
+                                        "callee": "Shopkeeper"
+                                    }
+                            }
+                        ]
+                }
+            ]
+    },
+    layout)
+
+    let elementKeys = Object.keys(sequenceDiagram.diagramDescription)
+    let lifelinesKeys = Object.keys(sequenceDiagram.lifelines)
+    if ((elementKeys.length == 1) && (lifelinesKeys.length == 2)) {
+        TestUtils.exportSVGToHTML(svg, __dirname + "/output/sequencediagramtests/SequenceDiagramCreateFromJSONTest3.html", true)
+
+        test.setOutputFilePath(__dirname + "/output/sequencediagramtests/SequenceDiagramCreateFromJSONTest3.html")
+        test.setReferenceFilePath(__dirname + "/reference/sequencediagramtests/SequenceDiagramCreateFromJSONTest3.html")
 
         resolve(tf.TestResultOutcome.ePassed)
     } else {
