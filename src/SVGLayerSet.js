12ed55ea068dad1dfae546066d1a3fe36f60a9c0
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-08-27 13:44:20 +0100

    Updates

--- src/SVGLayerSet.js
+++ src/SVGLayerSet.js
@@ -9,15 +9,33 @@ import { SVGLayer } from "./SVGLayer.js"
 */
 class SVGLayerSet {
 
+    /**
+      Creates a new SVGLayerSet instance.
+
+      @param {SVG} svg - The root SVG document.
+    */
     constructor(svg) {
         this.svg = svg
         this.layers = { }
     }
 
+    /**
+      Gets a layer.
+
+      @param {string} name - The name of the layer.
+      @returns {SVGLayer|null} The layer or null if no layer
+        with such name exists.
+    */
     getLayer(name) {
         return this.layers[name]
     }
 
+    /**
+      Creates a new layer.
+
+      @param {string} name - The name of the layer.
+      @returns {SVGLayer} The new layer.
+    */
     createLayer(name) {
         let newLayer = new SVGLayer(this.svg)
         this.layers[name] = newLayer
@@ -29,6 +47,8 @@ class SVGLayerSet {
       with the same name will be merged together
       with the elements of the set given as argument
       being appended.
+
+      @param {SVGLayerSet} layerSet - The other layer set.
     */
     merge(layerSet) {
         let self = this
@@ -38,6 +58,16 @@ class SVGLayerSet {
         })
     }
 
+    /**
+      Calls {@link SVGLayer#clear} on each layer in the set.
+    */
+    clearEachLayer() {
+        let self = this
+        let keys = Object.keys(self.layers)
+        keys.forEach(function(key) {
+            self.layers[key].clear()
+        })
+    }
 }
 
 export { SVGLayerSet }
