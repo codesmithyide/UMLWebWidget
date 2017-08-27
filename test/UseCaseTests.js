0db230842b117b63718bb9d6e0f331292666e68f
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-08-27 21:35:41 +0100

    Test updates

---
+++ test/UseCaseTests.js
@@ -0,0 +1,47 @@
+'use strict'
+
+var window = require("svgdom")
+var SVG = require("svg.js")(window)
+var UMLWebWidget = require("../dist/codesmithy-umlwebwidget.js")
+var tf = require("ishiko-test-framework")
+var TestUtils = require("./TestUtils.js")
+
+module.exports = function(theTestHarness) {
+    let useCaseSequence = theTestHarness.appendTestSequence("UseCase tests")
+
+    TestUtils.createDirectory(__dirname + "/output/usecasetests")
+
+    new tf.FunctionBasedTest("Creation test 1", UseCaseCreationTest1, useCaseSequence)
+
+    new tf.FileComparisonTest("getLayers test 1", UseCaseGetLayersTest1, useCaseSequence)
+}
+
+function UseCaseCreationTest1(resolve) {
+    let svg = SVG(window.document.createElement("div"))
+    let useCaseDescription = {
+        "title": "Pay for merchandise"
+    }
+    let useCase = new UMLWebWidget.UseCase(svg, useCaseDescription.title, useCaseDescription)
+    resolve(tf.TestResultOutcome.ePassed)
+}
+
+function UseCaseGetLayersTest1(resolve, reject, test) {
+    let svg = SVG(window.document.createElement("div"))
+
+    let useCaseDescription = {
+        "title": "Pay for merchandise"
+    }
+
+    let useCase = new UMLWebWidget.UseCase(svg, useCaseDescription.title, useCaseDescription)
+
+    let layers = useCase.getLayers()
+    layers.getLayer("shape").write()
+    layers.getLayer("text").write()
+
+    TestUtils.exportSVGToHTML(svg, __dirname + "/output/usecasetests/UseCaseGetLayersTest1.html", true)
+
+    test.setOutputFilePath(__dirname + "/output/usecasetests/UseCaseGetLayersTest1.html")
+    test.setReferenceFilePath(__dirname + "/reference/usecasetests/UseCaseGetLayersTest1.html")
+
+    resolve(tf.TestResultOutcome.ePassed)
+}
