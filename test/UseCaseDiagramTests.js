68de5c14dfbc6bd82f1598b193c186deb51d9124
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-08-28 12:24:47 +0100

    Test updates

--- test/UseCaseDiagramTests.js
+++ test/UseCaseDiagramTests.js
@@ -12,6 +12,7 @@ module.exports = function(theTestHarness) {
     TestUtils.createDirectory(__dirname + "/output/usecasediagramtests")
 
     new tf.FileComparisonTest("createFromJSON test 1", UseCaseDiagramCreateFromJSONTest1, useCaseDiagramSequence)
+    new tf.FileComparisonTest("createFromJSON test 2", UseCaseDiagramCreateFromJSONTest2, useCaseDiagramSequence)
 }
 
 function UseCaseDiagramCreateFromJSONTest1(resolve, reject, test) {
@@ -36,6 +37,47 @@ function UseCaseDiagramCreateFromJSONTest1(resolve, reject, test) {
 
         test.setOutputFilePath(__dirname + "/output/usecasediagramtests/UseCaseDiagramCreateFromJSONTest1.html")
         test.setReferenceFilePath(__dirname + "/reference/usecasediagramtests/UseCaseDiagramCreateFromJSONTest1.html")
+
+        resolve(tf.TestResultOutcome.ePassed)
+    } else {
+        resolve(tf.TestResultOutcome.eFailed)
+    }
+}
+
+function UseCaseDiagramCreateFromJSONTest2(resolve, reject, test) {
+    let svg = SVG(window.document.createElement("div"))
+    let useCaseDiagram = new UMLWebWidget.Diagram()
+    useCaseDiagram.createFromJSON(svg, {
+        "usecasediagram":
+          [
+              { 
+                  "actor":
+                      {
+                          "name": "Customer"
+                      }
+              },
+              { 
+                  "actor":
+                      {
+                          "name": "Shopkeeper"
+                      }
+              },
+              { 
+                  "actor":
+                      {
+                          "name": "Till"
+                      }
+              }
+          ]
+    })
+    let elementKeys = Object.keys(useCaseDiagram.diagramDescription)
+    let actorsKeys = Object.keys(useCaseDiagram.actors)
+    let usecasesKeys = Object.keys(useCaseDiagram.usecases)
+    if ((elementKeys.length == 1) && (actorsKeys.length == 3) && (usecasesKeys.length == 0)) {
+        TestUtils.exportSVGToHTML(svg, __dirname + "/output/usecasediagramtests/UseCaseDiagramCreateFromJSONTest2.html", true)
+
+        test.setOutputFilePath(__dirname + "/output/usecasediagramtests/UseCaseDiagramCreateFromJSONTest2.html")
+        test.setReferenceFilePath(__dirname + "/reference/usecasediagramtests/UseCaseDiagramCreateFromJSONTest2.html")
 
         resolve(tf.TestResultOutcome.ePassed)
     } else {
