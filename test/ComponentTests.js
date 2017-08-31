38d942859dd4bce50f3f99b425fb15087b39173f
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-08-31 20:03:39 +0100

    Test updates

--- test/ComponentTests.js
+++ test/ComponentTests.js
@@ -16,6 +16,7 @@ module.exports = function(theTestHarness) {
     new tf.FileComparisonTest("getLayers test 1", ComponentGetLayersTest1, componentSequence)
     new tf.FileComparisonTest("getLayers test 2", ComponentGetLayersTest2, componentSequence)
     new tf.FileComparisonTest("getLayers test 3", ComponentGetLayersTest3, componentSequence)
+    new tf.FileComparisonTest("getLayers test 4", ComponentGetLayersTest4, componentSequence)
 }
 
 function ComponentCreationTest1(resolve) {
@@ -28,6 +29,7 @@ function ComponentCreationTest1(resolve) {
     resolve(tf.TestResultOutcome.ePassed)
 }
 
+// Test a component without any interfaces or dependencies
 function ComponentGetLayersTest1(resolve, reject, test) {
     let svg = SVG(window.document.createElement("div"))
 
@@ -50,6 +52,7 @@ function ComponentGetLayersTest1(resolve, reject, test) {
     resolve(tf.TestResultOutcome.ePassed)
 }
 
+// Test a component with a single dependency and no interfaces
 function ComponentGetLayersTest2(resolve, reject, test) {
     let svg = SVG(window.document.createElement("div"))
 
@@ -76,6 +79,7 @@ function ComponentGetLayersTest2(resolve, reject, test) {
     resolve(tf.TestResultOutcome.ePassed)
 }
 
+// Test a component with a single interface and no dependencies
 function ComponentGetLayersTest3(resolve, reject, test) {
     let svg = SVG(window.document.createElement("div"))
 
@@ -98,6 +102,37 @@ function ComponentGetLayersTest3(resolve, reject, test) {
 
     test.setOutputFilePath(__dirname + "/output/componenttests/ComponentGetLayersTest3.html")
     test.setReferenceFilePath(__dirname + "/reference/componenttests/ComponentGetLayersTest3.html")
+
+    resolve(tf.TestResultOutcome.ePassed)
+}
+
+// Test a component with a single interface and a single dependency
+function ComponentGetLayersTest4(resolve, reject, test) {
+    let svg = SVG(window.document.createElement("div"))
+
+    let componentDescription = {
+        "name": "WebServer",
+            "dependencies":
+                [
+                    { "name": "INetwork" }
+                ],
+            "interfaces":
+                [
+                    { "name": "IControl" }
+                ]
+    }
+
+    let style = new UMLWebWidget.Style()
+    let component = new UMLWebWidget.Component(svg, componentDescription.name, componentDescription, style)
+
+    let layers = component.getLayers()
+    layers.getLayer("shape").write()
+    layers.getLayer("text").write()
+
+    TestUtils.exportSVGToHTML(svg, __dirname + "/output/componenttests/ComponentGetLayersTest4.html", true)
+
+    test.setOutputFilePath(__dirname + "/output/componenttests/ComponentGetLayersTest4.html")
+    test.setReferenceFilePath(__dirname + "/reference/componenttests/ComponentGetLayersTest4.html")
 
     resolve(tf.TestResultOutcome.ePassed)
 }
