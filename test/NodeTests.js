d90ea8266e28babd6565da54a29e912a2f628a27
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-09-02 14:04:52 +0100

    Added Node tests

---
+++ test/NodeTests.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var UMLWebWidget = require("../dist/codesmithy-umlwebwidget.js")
+var tf = require("ishiko-test-framework")
+
+module.exports = function(theTestHarness) {
+    let nodeSequence = theTestHarness.appendTestSequence("Node tests")
+}
