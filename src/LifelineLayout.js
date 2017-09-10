9094179222cf2633cf007dbf5656f9e4b332596f
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-09-10 12:17:09 +0100

    Refactoring

--- src/LifelineLayout.js
+++ src/LifelineLayout.js
@@ -10,73 +10,73 @@ class LifelineLayout {
             let connectionPoint = connectionPoints[i]
             switch (connectionPoint.type) {
                 case "synchronous-start":
-                    this.addCallerOccurrence(levels, connectionPoint.point.y)
+                    addCallerOccurrence(levels, connectionPoint.point.y)
                     break
 
                 case "synchronous-end":
-                    this.addCalleeOccurrence(levels, connectionPoint.point.y)
+                    addCalleeOccurrence(levels, connectionPoint.point.y)
                     break
 
                 case "return-start":
-                    this.addReturnOccurrence(levels, connectionPoint.point.y)
+                    addReturnOccurrence(levels, connectionPoint.point.y)
                     break
 
                 case "return-end":
-                    this.addReturnCalleeOccurrence(levels, connectionPoint.point.y)
+                    addReturnCalleeOccurrence(levels, connectionPoint.point.y)
                     break
 
                 case "creation-start":
-                    this.addCallerOccurrence(levels, connectionPoint.point.y)
+                    addCallerOccurrence(levels, connectionPoint.point.y)
                     break
 
                 case "destruction-end":
                     if (adjustmentNeeded) {
-                        this.addReturnOccurrence(levels, connectionPoint.point.y - 25)
+                        addReturnOccurrence(levels, connectionPoint.point.y - 25)
                     }
-                    this.addDestructionOccurrence(levels, connectionPoint.point.y)
+                    addDestructionOccurrence(levels, connectionPoint.point.y)
                     break
             }
         }
     }
 
-    addCallerOccurrence(levels, y) {
+}
+
+function addCallerOccurrence(levels, y) {
+    levels.push([y, 1])
+    concatenateLevels(levels)
+}
+
+function addCalleeOccurrence(levels, y) {
+    if (levels.length == 0) {
         levels.push([y, 1])
-        concatenateLevels(levels)
+    } else {
+        levels.push([y, levels[levels.length - 1][1] + 1])
     }
+    concatenateLevels(levels)
+}
 
-    addCalleeOccurrence(levels, y) {
-        if (levels.length == 0) {
-            levels.push([y, 1])
-        } else {
-            levels.push([y, levels[levels.length - 1][1] + 1])
-        }
-        concatenateLevels(levels)
+function addReturnOccurrence(levels, y) {
+    let newLevel = 0
+    let length = levels.length
+    if (length > 0) {
+        newLevel = Math.max(0, (levels[length - 1][1] - 1))
     }
+    levels.push([y, newLevel])
+    concatenateLevels(levels)
+}
 
-    addReturnOccurrence(levels, y) {
-        let newLevel = 0
-        let length = levels.length
-        if (length > 0) {
-            newLevel = Math.max(0, (levels[length - 1][1] - 1))
-        }
-        levels.push([y, newLevel])
-        concatenateLevels(levels)
+function addReturnCalleeOccurrence(levels, y) {
+    let length = levels.length
+    if (length == 0) {
+        levels.push([y, 1])
+    } else {
+        levels.push([y, levels[length - 1][1]])
     }
+    concatenateLevels(levels)
+}
 
-    addReturnCalleeOccurrence(levels, y) {
-        let length = levels.length
-        if (length == 0) {
-            levels.push([y, 1])
-        } else {
-            levels.push([y, levels[length - 1][1]])
-        }
-        concatenateLevels(levels)
-    }
-
-    addDestructionOccurrence(levels, y) {
-        levels.push([y, 0])
-    }
-
+function addDestructionOccurrence(levels, y) {
+    levels.push([y, 0])
 }
 
 function concatenateLevels(levels) {
