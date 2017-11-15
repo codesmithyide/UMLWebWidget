4489d145ec7dcb1a04dd4c8d29f0e54e776da3d9
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-11-15 21:03:05 +0000

    Test updates

--- test/ClassTemplateTests.js
+++ test/ClassTemplateTests.js
@@ -14,6 +14,7 @@ module.exports = function(theTestHarness) {
     new tf.FunctionBasedTest("Creation test 1", ClassTemplateCreationTest1, classtemplateSequence)
 
     new tf.FileComparisonTest("getLayers test 1", ClassTemplateGetLayersTest1, classtemplateSequence)
+    new tf.FileComparisonTest("getLayers test 2", ClassTemplateGetLayersTest2, classtemplateSequence)
 }
 
 function ClassTemplateCreationTest1(resolve) {
@@ -56,6 +57,34 @@ function ClassTemplateGetLayersTest1(resolve, reject, test) {
 
     test.setOutputFilePath(__dirname + "/output/classtemplatetests/ClassTemplateGetLayersTest1.html")
     test.setReferenceFilePath(__dirname + "/reference/classtemplatetests/ClassTemplateGetLayersTest1.html")
+
+    resolve(tf.TestResultOutcome.ePassed)
+}
+
+function ClassTemplateGetLayersTest2(resolve, reject, test) {
+    let svg = SVG(window.document.createElement("div"))
+
+    let classTemplateDescription = {
+        "name": "MyClassTemplate",
+        "parameters": [ "T" ],
+        "attributes":
+            [
+            ],
+        "operations":
+            [
+            ]
+    }
+    let style = new UMLWebWidget.Style()
+    let classtemplate = new UMLWebWidget.ClassTemplate(svg, classTemplateDescription.name, classTemplateDescription, style)
+
+    let layers = classtemplate.getLayers()
+    layers.layers["shape"].write()
+    layers.layers["text"].write()
+
+    TestUtils.exportSVGToHTML(svg, __dirname + "/output/classtemplatetests/ClassTemplateGetLayersTest2.html", true)
+
+    test.setOutputFilePath(__dirname + "/output/classtemplatetests/ClassTemplateGetLayersTest2.html")
+    test.setReferenceFilePath(__dirname + "/reference/classtemplatetests/ClassTemplateGetLayersTest2.html")
 
     resolve(tf.TestResultOutcome.ePassed)
 }
