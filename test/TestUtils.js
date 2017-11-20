adccaa4a1d988cc75818bfe604a7f19781b204bd
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-11-20 23:01:08 +0000

    Test updates

--- test/TestUtils.js
+++ test/TestUtils.js
@@ -21,7 +21,7 @@ function createDirectory(path) {
 function exportSVGToHTML(svg, path, stylesheet = false) {
     let data = ""
     if (stylesheet) {
-        data = "<!DOCTYPE html><html><head><link rel=\"stylesheet\" href=\"../../../StyleSheets/CodeSmithyUMLWebWidget.css\"></link></head><body><div class=\"CodeSmithyUMLWebWidget\">" + svg.svg() + "</div></body></html>"
+        data = "<!DOCTYPE html><html><head><link rel=\"stylesheet\" href=\"../../../styleSheets/codesmithy-umlwebwidget.css\"></link></head><body><div class=\"CodeSmithyUMLWebWidget\">" + svg.svg() + "</div></body></html>"
     } else {
         data = "<!DOCTYPE html><html><body><div>" + svg.svg() + "</div></body></html>"
     }
