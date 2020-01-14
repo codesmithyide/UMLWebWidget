1c1543baaf64b735d903dc712b97f21a96e277c4
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-01-14 20:51:22 +0000

    Refactoring

---
+++ webpack.common.js
@@ -0,0 +1,11 @@
+var path = require("path")
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        library: "CodeSmithyUMLWebWidget",
+        libraryTarget: "umd",
+        filename: "codesmithy-umlwebwidget.js",
+        path: path.resolve(__dirname, "dist")
+    },
+}
