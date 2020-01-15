3c2f80607ca184eeb0dce516460f0e6e5efa7796
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-01-15 01:42:54 +0000

    Add TypeScript support

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,17 @@ var path = require("path")
 
 module.exports = {
     entry: "./src/index.js",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.ts', '.js' ]
+    },
     output: {
         library: "CodeSmithyUMLWebWidget",
         libraryTarget: "umd",
