701812e3b078113e37c3b21aae53587e94d5d404
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-02-18 02:15:55 +0000

    Remove unnecessary IDs on ClassBox (#45)

--- src/ClassBox.ts
+++ src/ClassBox.ts
@@ -4,6 +4,7 @@ import { DiagramElement } from "./DiagramElement"
 import { SVGLayer } from "./SVGLayer"
 import { Style } from "./Style"
 import { ConnectionPoint } from "./ConnectionPoint"
+import { SVGUtils } from "./SVGUtils"
 import { DrawingUtilities } from "./DrawingUtilities"
 
 /** 
@@ -106,10 +107,13 @@ function createDef(self, classInfo, canMove, style) {
     }
 
     currentDimensions.width += (style.getLeftMargin("classbox") + style.getRightMargin("classbox"))
-    
-    let rect = classGroup.rect(currentDimensions.width, currentDimensions.height).move(borderAdjustment.left, borderAdjustment.top)
-    classGroup.line(borderAdjustment.left, line1YPos, borderAdjustment.left + currentDimensions.width, line1YPos)
-    classGroup.line(borderAdjustment.left, line2YPos, borderAdjustment.left + currentDimensions.width, line2YPos)
+
+    let rect = SVGUtils.Rectangle(classGroup, borderAdjustment.left, borderAdjustment.top, currentDimensions.width,
+        currentDimensions.height)
+    SVGUtils.Line(classGroup, borderAdjustment.left, line1YPos, borderAdjustment.left + currentDimensions.width,
+        line1YPos)
+    SVGUtils.Line(classGroup, borderAdjustment.left, line2YPos, borderAdjustment.left + currentDimensions.width,
+        line2YPos)
 
     self.connectionPointsRectangle = rect.bbox()
 
