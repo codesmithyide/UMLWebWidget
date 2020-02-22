91cc12827223ec64a1c9a4050042da08ddffdb18
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-02-22 19:25:56 +0000

    Add assertAlways tests

--- test/ErrorsTests.ts
+++ test/ErrorsTests.ts
@@ -10,11 +10,38 @@ var UMLWebWidget = require("../dist/codesmithy-umlwebwidget.node.dev.js")
 var tf = require("ishiko-test-framework")
 
 module.exports = function(theTestHarness) {
-    let errorSequence = theTestHarness.appendTestSequence("UMLWebWidgetError tests")
+    let errorSequence = theTestHarness.appendTestSequence("Errors tests")
 
-    new tf.FunctionBasedTest("Creation test 1", UMLWebWidgetErrorCreationTest1, errorSequence)
+    new tf.FunctionBasedTest("Errors creation test 1", ErrorsCreationTest1, errorSequence)
+    new tf.FunctionBasedTest("assertAlways test 1", ErrorsAssertAlwaysTest1, errorSequence)
+    new tf.FunctionBasedTest("assertAlways test 2", ErrorsAssertAlwaysTest2, errorSequence)
+    new tf.FunctionBasedTest("UMLWebWidgetError creation test 1", UMLWebWidgetErrorCreationTest1, errorSequence)
+    new tf.FunctionBasedTest("UMLWebWidgetError try/catch test 1", UMLWebWidgetErrorTryCatchTest1, errorSequence)
+}
 
-    new tf.FunctionBasedTest("try/catch test 1", UMLWebWidgetErrorTryCatchTest1, errorSequence)
+function ErrorsCreationTest1(resolve) {
+    let errors = new UMLWebWidget.Errors()
+    if (errors instanceof UMLWebWidget.Errors) {
+        resolve(tf.TestResultOutcome.ePassed)
+    } else {
+        resolve(tf.TestResultOutcome.eFailed)
+    }
+}
+
+function ErrorsAssertAlwaysTest1(resolve) {
+    let errors = new UMLWebWidget.Errors()
+    errors.assertAlways(true);
+    resolve(tf.TestResultOutcome.ePassed)
+}
+
+function ErrorsAssertAlwaysTest2(resolve) {
+    let errors = new UMLWebWidget.Errors()
+    try {
+        errors.assertAlways(false);
+        resolve(tf.TestResultOutcome.eFailed)
+    } catch(e) {
+        resolve(tf.TestResultOutcome.ePassed)
+    }
 }
 
 function UMLWebWidgetErrorCreationTest1(resolve) {
