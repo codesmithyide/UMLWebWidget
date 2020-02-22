8e57344c82a09453ee9204c6f2bc161c7b6b9720
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-02-22 19:46:30 +0000

    Implement assertions (#53)

--- src/Settings.ts
+++ src/Settings.ts
@@ -7,23 +7,36 @@
 'use strict'
 
 /**
-  Diagram settings.
+ * The type of build.
+ */
+const enum BuildType {
+    /**
+     * In debug mode errors are reported via window.alert if supported and considered fatal.
+     */
+    DEBUG,
+    /**
+     * In release mode errors are reported to the console but are otherwise ignored unless they can't be recovered
+     * from.
+     */
+    RELEASE
+}
 
-  @property {int} width - The width of the diagram in pixels.
-  @property {height} height - The height of the diagram in pixels.
-  @property {boolean} debug - If debug is true then additional checks
-    and logging will be performed. This is false by default so that
-    diagrams are displayed as well as possible regardless
-    of errors. It is recommended to enable debug mode when updating a
-    diagram and set it back to false afterwards.
-*/
+/**
+ * Diagram settings.
+ *
+ * @property {int} width - The width of the diagram in pixels.
+ * @property {height} height - The height of the diagram in pixels.
+ * @property {boolean} debug - If debug is true then additional checks and logging will be performed. This is false by
+ *     default so that diagrams are displayed as well as possible regardless of errors. It is recommended to enable
+ *     debug mode when updating a diagram and set it back to false afterwards.
+ */
 class Settings {
     width: number
     height: number
     canMove: boolean
     canResize: boolean
     logLevel
-    debug
+    buildType: BuildType
 
     /** 
       Creates a new Settings instance with each property
@@ -40,7 +53,7 @@ class Settings {
         this.canMove = false
         this.canResize = false
         this.logLevel = "none"
-        this.debug = false
+        this.buildType = BuildType.RELEASE
 
         if (jsonSettings) {
             if (jsonSettings.width) {
@@ -59,7 +72,6 @@ class Settings {
             }
         }
     }
-
 }
 
-export { Settings }
+export { BuildType, Settings }
