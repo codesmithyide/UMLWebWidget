5e69890f436b22c23ba3bf56fd2a8291b37a8ae8
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-02-22 19:44:03 +0000

    Implement assertions

--- src/Errors.ts
+++ src/Errors.ts
@@ -6,14 +6,39 @@
 
 'use strict'
 
-/**
-  Errors related to diagram operations are
-  communicated via this class.
+import { BuildType } from "./Settings"
 
-  Note that by default errors are only shown
-  if debug mode is enabled in the {@link Settings}.
-*/
+/**
+ * Class that provides methods to handle errors.
+ */
+class Errors {
+    buildType: BuildType
+
+    constructor(buildType: BuildType) {
+        this.buildType = buildType
+    }
+
+    assertAlways(condition: boolean) {
+        if (!condition) {
+            console.error("Assertion failed")
+            window.alert("Assertion failed")
+            throw new Error()
+        }
+    }
+
+    assertDebug(condition: boolean) {
+        if (this.buildType == BuildType.DEBUG) {
+            this.assertAlways(condition)
+        }
+    }
+}
+
+/**
+ * Errors related to diagram operations are communicated via this class.
+ *
+ * Note that by default errors are only shown if debug mode is enabled in the {@link Settings}.
+ */
 class UMLWebWidgetError extends Error {    
 }
 
-export { UMLWebWidgetError }
+export { Errors, UMLWebWidgetError }
--- test/ErrorsTests.ts
+++ test/ErrorsTests.ts
@@ -15,12 +15,15 @@ module.exports = function(theTestHarness) {
     new tf.FunctionBasedTest("Errors creation test 1", ErrorsCreationTest1, errorSequence)
     new tf.FunctionBasedTest("assertAlways test 1", ErrorsAssertAlwaysTest1, errorSequence)
     new tf.FunctionBasedTest("assertAlways test 2", ErrorsAssertAlwaysTest2, errorSequence)
+    new tf.FunctionBasedTest("assertDebug test 1", ErrorsAssertDebugTest1, errorSequence)
+    new tf.FunctionBasedTest("assertDebug test 2", ErrorsAssertDebugTest2, errorSequence)
     new tf.FunctionBasedTest("UMLWebWidgetError creation test 1", UMLWebWidgetErrorCreationTest1, errorSequence)
     new tf.FunctionBasedTest("UMLWebWidgetError try/catch test 1", UMLWebWidgetErrorTryCatchTest1, errorSequence)
 }
 
 function ErrorsCreationTest1(resolve) {
-    let errors = new UMLWebWidget.Errors()
+    let settings = new UMLWebWidget.Settings()
+    let errors = new UMLWebWidget.Errors(settings.buildType)
     if (errors instanceof UMLWebWidget.Errors) {
         resolve(tf.TestResultOutcome.ePassed)
     } else {
@@ -29,15 +32,37 @@ function ErrorsCreationTest1(resolve) {
 }
 
 function ErrorsAssertAlwaysTest1(resolve) {
-    let errors = new UMLWebWidget.Errors()
+    let settings = new UMLWebWidget.Settings()
+    let errors = new UMLWebWidget.Errors(settings.buildType)
     errors.assertAlways(true);
     resolve(tf.TestResultOutcome.ePassed)
 }
 
 function ErrorsAssertAlwaysTest2(resolve) {
-    let errors = new UMLWebWidget.Errors()
+    let settings = new UMLWebWidget.Settings()
+    let errors = new UMLWebWidget.Errors(settings.buildType)
     try {
         errors.assertAlways(false);
+        resolve(tf.TestResultOutcome.eFailed)
+    } catch(e) {
+        resolve(tf.TestResultOutcome.ePassed)
+    }
+}
+
+function ErrorsAssertDebugTest1(resolve) {
+    let settings = new UMLWebWidget.Settings()
+    settings.buildType = 0
+    let errors = new UMLWebWidget.Errors(settings.buildType)
+    errors.assertDebug(true);
+    resolve(tf.TestResultOutcome.ePassed)
+}
+
+function ErrorsAssertDebugTest2(resolve) {
+    let settings = new UMLWebWidget.Settings()
+    settings.buildType = 0
+    let errors = new UMLWebWidget.Errors(settings.buildType)
+    try {
+        errors.assertDebug(false);
         resolve(tf.TestResultOutcome.eFailed)
     } catch(e) {
         resolve(tf.TestResultOutcome.ePassed)
